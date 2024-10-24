b7d8547a6cdd041ab5b4f5eff81d123dfbc50fe7
Blue Tide Chapel <pastor@bluetidechapel.com>
2024-10-24 14:25:48 -0400

    remove minify

--- minify.js
+++
@@ -1,6 +0,0 @@
-function minifyCSS(css) {
-  return css.replace(/\s+/g, ' ') 
-            .replace(/\/\*[^*]*\*+([^/*][^*]*\*+)*\//g, '') 
-            .replace(/\s*([{}:;,])\s*/g, '$1')
-            .replace(/;}/g, '}'); 
-}
